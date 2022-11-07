06796d309829babd17e5752ca74a72762790668d
박은빈 <joypeb@naver.com>
2022-11-07 11:45:51 +0900

    mustache controller, greeting.mustache

---
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -0,0 +1,23 @@
+package com.mustache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username","hi");
+        model.addAttribute("id","0");
+        return "greeting";
+    }
+
+    @GetMapping(value = "/hi2")
+    public String mustacheCon2(Model model) {
+        model.addAttribute("username","hi2");
+        model.addAttribute("id","1");
+        return "greeting";
+    }
+}
